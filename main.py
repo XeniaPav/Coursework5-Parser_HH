9af970409b2d769bc232c8b35c60b990a51853d2
Pavlukhina Ksenia <kseny100297@yandex.ru>
2024-06-24 23:00:28 +0500

    add uploading information and creating tables

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from src.get_vacancy import get_company, get_vacancies, get_vacancies_list
+from src.config import config
+from src.ulils import create_database, save_data_to_database
+import psycopg2
 
-def main():
-    pass
 
+if __name__ == '__main__':
+    params = config()
+    data = get_vacancies(get_company())
+    vacancies = get_vacancies_list(data)
+    create_database("vacancy", params)
+    save_data_to_database(vacancies, "vacancy", params)
 
-if __name__ == main():
