10615716ccf32a60679c96f2a50152c6f6dc90b5
Pavlukhina Ksenia <kseny100297@yandex.ru>
2024-06-29 18:02:55 +0500

    add tests

---
+++ tests/test_DB_class.py
@@ -0,0 +1,19 @@
+from src.DB_class import DBManager
+from src.config import config
+
+params = config(filename="database_test.ini", section="postgresql")
+prog = DBManager("vacancy", params)
+
+
+def test_prog_get_companies_and_vacancies_count():
+    answer = prog.get_companies_and_vacancies_count()
+    assert len(answer) == 15
+
+def test_prog_get_all_vacancies():
+    answer = prog.get_all_vacancies()
+    assert answer != None
+
+
+def test_prog_get_avg_salary():
+    answer = prog.get_avg_salary()
+    assert answer != 1156
